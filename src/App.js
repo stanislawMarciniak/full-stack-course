d11da284f5db95a54930be30ea9bcd4e212cd62a
Stanisław Marciniak <marciniak13ee@gmail.com>
2023-02-21 21:28:49 +0100

    part2

--- src/App.js
+++ src/App.js
@@ -1,46 +1,17 @@
-import { useState } from 'react'
+import Note from './components/Note'
 
-const App = () => {
-  const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-    'The only way to go fast, is to go well.'
-  ]
-   
-  const [selected, setSelected] = useState(0)
-  const [votes, setVotes] = useState([0, 0, 0, 0, 0, 0, 0, 0])
-
-  const copy = [...votes]
-
-  let max = copy[0]
-  let maxIndex = 0
-
-  for(var i = 0; i < copy.length; i++) {
-    if (max < copy[i]){
-        max = copy[i]
-        maxIndex = i
-      }
-  }
-
-  copy[selected] += 1
-
+const App = ({ notes }) => {
   return (
     <div>
-      <h1>Anecdote of the day</h1>
-      {anecdotes[selected]} <br />
-      <button onClick={() => setVotes(copy)}>vote</button>
-      <button onClick={() => setSelected(Math.floor(Math.random() * anecdotes.length))}>next anecdote</button> <br />
-      has {votes[selected]} votes <br />
-      <h1>Anecdote with most votes</h1>
-      {anecdotes[maxIndex]} <br />
-      has {max} votes <br />
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => 
+          <Note key={note.id} note={note} /> 
+        )}
+      </ul>
     </div>
   )
 }
+
 
 export default App
