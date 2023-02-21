0a6462ef13005647e4db1d703ad23d24eb62880b
Stanisław Marciniak <marciniak13ee@gmail.com>
2023-02-21 23:34:44 +0100

    2.1-2.5

--- src/App.js
+++ src/App.js
@@ -1,17 +1,58 @@
-import Note from './components/Note'
+import Course from "./components/Course"
 
-const App = ({ notes }) => {
+const App = () => {
+  const courses = [
+    {
+      id: 1,
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }, 
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
   return (
-    <div>
-      <h1>Notes</h1>
-      <ul>
-        {notes.map(note => 
-          <Note key={note.id} note={note} /> 
-        )}
-      </ul>
-    </div>
+    <>
+      <h1>Web development curriculum</h1>
+      {courses.map(course => 
+        <Course key={course.id} course={course} />)}
+    </>
   )
 }
-
 
 export default App
