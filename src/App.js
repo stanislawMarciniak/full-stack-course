518f12a56b76e19b4e37bba0f5775a02cc6656fc
stasiek <marciniak13ee@gmail.com>
2023-02-27 13:21:57 +0100

    2.5

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-const Hello = (props) => {
-  console.log(props)
+import { useState } from 'react'
+
+import Name from './components/Name'
+
+const Names = ({persons}) => {
   return (
-    <div>
-      <p>
-        Hello {props.name}, you are {props.age} years old
-        </p>
-    </div>
+    <ul>
+        {persons.map(person => <li>{person.content}</li>)}
+    </ul>
   )
 }
 
 const App = () => {
-  const name = 'Peter'
-  const age = 10
+  const [persons, setPersons] = useState([
+    { 
+      content: 'Arto Hellas' ,
+      id: 1,
+    }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      content: newName,
+      id: persons.length + 1,
+    }
+
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  const handlePersonChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
 
   return (
     <div>
-      <h1>Greetings</h1> 
-      <Hello name = 'Maya' age = {26 + 10} />
-      <Hello name = {name} age = {age} />
+      <div>debug: {newName}</div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+          name: 
+          <input 
+            value={newName}
+            onChange={handlePersonChange}
+          />
+          <button type="submit">add</button>
+      </form>
+      <h2>Numbers</h2>
+      <Names persons={persons}/>
     </div>
   )
 }
