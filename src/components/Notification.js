6da90357f3ab7ac76c3abc9a1fe1fb64a873cf23
Stanisław Marciniak <marciniak13ee@gmail.com>
2023-03-13 23:38:13 +0100

    2.17

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,8 +1,12 @@
-const Notification = ({ message }) => {
+const Notification = ({ message, type }) => {
+  console.log(`notification type: ${type}`);
   if (message === null) {
     return null;
   }
-  return <div className="notification">{message}</div>;
-  return <div className="error">{message}</div>;
+  if (type === "notification") {
+    return <div className="notification">{message}</div>;
+  } else if (type === "error") {
+    return <div className="error">{message}</div>;
+  }
 };
 export default Notification;
