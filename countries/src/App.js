a945723c42879450f025a177ab22ab4aea7addbd
Stanislaw Marciniak <marciniak13ee@gmail.com>
2023-03-16 13:56:35 +0100

    countries

--- countries/src/App.js
+++ countries/src/App.js
@@ -1,8 +1,23 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 function App() {
+  const [countries, setCountries] = useState(null);
+
+  useEffect(
+    () =>
+      axios.get(`https://restcountries.com/v3.1/all`).then((response) => {
+        setCountries(response.data);
+      }),
+    []
+  );
+
   return (
     <div>
-      find country
-      <input />
+      <form>
+        find country
+        <input value={value} onChange={handleChange} />
+      </form>
     </div>
   );
 }
